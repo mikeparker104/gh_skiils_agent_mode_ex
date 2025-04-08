a1151ced5111f8aaead30e728ccb94266b361b29
Mike Parker <mikeparker104@users.noreply.github.com>
2025-04-08 12:01:43 +0000

    Renamed 'components' folder to 'js' and updated references

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Activities from './components/Activities';
-import Leaderboard from './components/Leaderboard';
-import Teams from './components/Teams';
-import Users from './components/Users';
-import Workouts from './components/Workouts';
+import Activities from './js/Activities';
+import Leaderboard from './js/Leaderboard';
+import Teams from './js/Teams';
+import Users from './js/Users';
+import Workouts from './js/Workouts';
 
 function App() {
   useEffect(() => {
